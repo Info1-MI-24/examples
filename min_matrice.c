b5a3b1a76ce2cd5dbb5cc7da53dc36d22b054c6c
Maulaz Tony <tony.maulaz@heig-vd.ch>
2024-12-18 13:38:23 +0100

    Add chaine de caractère

--- min_matrice.c
+++ min_matrice.c
@@ -1,6 +1,13 @@
 // Développer une fonction qui peremet de trouver le minimum et le maximum d'une matrice
 
+// Développer une fonction de type bool qui permet de trouver la première occurence d'un caractère dans une chaîne.
+// Si le caractère n'est pas trouvé, la fonction retourne false;
+// Sinon, elle retourne true et met à jour la position du caractère dans la variable passée en paramètre.
+// La fonction est insensitive à la casse.
+// Afficher la chaîne de caractère depuis la position trouvée jusqu'à la fin de la chaîne.
+
 #include <stdio.h>
+#include <stdbool.h>
 
 #define SIZE_COL 3
 #define SIZE_ROW 3
@@ -19,6 +26,23 @@ int main(){
  
     printf("Min: %d\n", min);
     printf("Max: %d\n", max);
+
+
+    char chaine[] = "Bonjour, comment allez-vous?";
+
+    int position;
+    char caractere = 'V';
+
+    // appel de la fonction
+    bool is_found = true;
+
+    if( is_found ){
+        printf("Le caractère %c a été trouvé à la position %d\n", caractere, position);
+        printf("La chaîne à partir de la position %d est: %s\n", position, "");
+    }
+    else{
+        printf("Le caractère %c n'a pas été trouvé\n", caractere);
+    }
 
     return 0;
 }
